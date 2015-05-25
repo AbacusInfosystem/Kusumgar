9b98f085f522b1f6e29d29229cca0b681c18e4a5
rupali-galande <rupaligalande01@gmail.com>
2015-05-25 20:16:39 +0530

    free commit

---
+++ Kusumgar/Controllers/PostLogin/Master/ProductVendorController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Kusumgar.Models;
+using KusumgarBusinessEntities;
+using KusumgarDatabaseEntities;
+using KusumgarModel;
+using KusumgarBusinessEntities.Common;
+using KusumgarHelper.PageHelper;
+using Kusumgar.Models;
+
+namespace Kusumgar.Controllers.PostLogin.Master
+{
+    public class ProductVendorController : Controller
+    {
+        
+        // public ProductVendorManager _productManager { get; set; }
+
+        //public ActionResult Index(ProductVendorViewModel pvViewModel)
+        //{
+        //    return View("Index", pvViewModel);
+        //}
+        // public ActionResult Insert(ProductVendorViewModel pvViewModel)
+        // {
+        //     _productManager.Insert_Product_Vendor(pvViewModel.Product_Vendor);
+        //     return Json(pvViewModel);
+        // }
+    }
+}
+
