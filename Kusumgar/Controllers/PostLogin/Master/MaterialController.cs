708e888739ee07d19c653ed70a193bfbc014f102
kaustubh-chaudhari <kaustubh.chaudhari@abacusinfosystem.com>
2015-06-02 18:57:08 +0530

    Free Commit

--- Kusumgar/Controllers/PostLogin/Master/MaterialController.cs
+++ Kusumgar/Controllers/PostLogin/Master/MaterialController.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-using Kusumgar.Models;
+using Kusumgar.Models; 
 using KusumgarBusinessEntities;
 using KusumgarDatabaseEntities;
 using KusumgarModel;
