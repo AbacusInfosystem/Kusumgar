370438cd030f0a8c04088c65e991e00e4f841d05
kaustubh-chaudhari <kaustubh.chaudhari@abacusinfosystem.com>
2015-06-05 16:04:18 +0530

    Test Unit Info Updated

--- KusumgarBusinessEntities/Master/TestUnitInfo.cs
+++ KusumgarBusinessEntities/Master/TestUnitInfo.cs
@@ -12,13 +12,31 @@ namespace KusumgarBusinessEntities
     {
       public TestUnitInfo()
       {
-            TestUnitEntity =new M_Test_Unit();
+            //TestUnitEntity =new M_Test_Unit();
        
        }
-      public M_Test_Unit TestUnitEntity { get; set; }
-      
-        public string Label { get; set; }
+    //  public M_Test_Unit TestUnitEntity { get; set; }
+
+      public int Test_Unit_Id { get; set; }
+
+      public string Test_Unit_Name { get; set; }
+
+      public bool Status { get; set; }
+
+      public DateTime CreatedOn { get; set; }
+
+      public int CreatedBy { get; set; }
+
+      public DateTime UpdatedOn { get; set; }
+
+      public int UpdatedBy { get; set; }
+
+      #region Additional Fields
+
+      public string Label { get; set; }
         
         public int Value { get; set; }
+
+      #endregion
     }
 }
